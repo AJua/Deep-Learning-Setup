dc93dc04106a8e9b8b001ba9c30e312cace55493
AJua <ljredracess@gmail.com>
2016-08-26 23:58:21 +0800

    auto accpet yes

--- install.sh
+++ install.sh
@@ -3,13 +3,11 @@
 apt-get update
 add-apt-repository ppa:aseering/wsl
 apt-get update
-apt-get install python-pip ipython ipython-notebook python-dev
-ipython notebook --no-browser
-
-apt-get install python-numpy python-scipy python-matplotlib python-pandas python-sympy python-nose
-
-pip install pyyaml h5py
+apt-get -y install python-numpy python-scipy python-matplotlib python-pandas python-sympy python-nose
+yes yes | pip install pyyaml h5py
 
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
-pip install git+git://github.com/fchollet/keras.git 
+yes yes | pip install --upgrade $TF_BINARY_URL
+yes yes | pip install git+git://github.com/fchollet/keras.git 
+
+ipython notebook --no-browser
