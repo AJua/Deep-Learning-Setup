643b429629dd7208d09875da987a8008b44aefa3
AJua <b91502086@gmail.com>
2016-08-28 06:39:52 +0800

    add conda install

---
+++ cpu-conda-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+wget http://repo.continuum.io/miniconda/Anaconda3-4.1.1-Linux-x86_64.sh -O ~/Anaconda3.sh
+bash ~/Anaconda3.sh -b -p $HOME/Anaconda3
+export PATH="$HOME/Anaconda3/bin:$PATH"
+echo 'export PATH="$HOME/Anaconda3/bin:$PATH"' >> ~/.bashrc
+
+conda install -y jzuhone zeromq=4.1.dev0
+conda install -y alchayward keras=0.1.2
+conda install -y conda-forge tensorflow
+
+mkdir ~/.keras
+cp ./keras.json ~/.keras/keras.json
+
+jupyter nbextension enable --py --sys-refix widgetsnbextension
+
+cd examples
+git submodule update --init
+cd Days-on-Jupyter
+jupyter notebook --no-browser
