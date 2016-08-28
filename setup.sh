6552556820c25f503d4029a1d2d82fe14e50637c
AJua <b91502086@gmail.com>
2016-08-28 03:57:02 +0100

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+apt-get update
+apt-get install git -y
+
 wget https://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh -O ~/Anaconda3.sh
 bash ~/Anaconda3.sh -b -p $HOME/Anaconda3
+
 export PATH="$HOME/Anaconda3/bin:$PATH"
+export DISPLAY=:0
 echo 'export PATH="$HOME/Anaconda3/bin:$PATH"' >> ~/.bashrc
+echo 'export DISPLAY=:0' >> ~/.bashrc
 
 conda install -c jzuhone zeromq=4.1.dev0 -y
 conda install -c conda-forge tensorflow -y
-
+pip install git+git://github.com/fchollet/keras.git
 mkdir ~/.keras
 cp ./keras.json ~/.keras/keras.json
 
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
-cd examples
-git submodule update --init
-cd Days-on-Jupyter
+git clone https://github.com/yenlung/Days-on-Jupyter.git ~/Days-on-Jupyter
+cd ~/Days-on-Jupyter
 jupyter notebook --no-browser
